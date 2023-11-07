114cf2f4b3fadc2cce9337d72726569aecd97000
FynnHer <133028317+FynnHer@users.noreply.github.com>
2023-11-07 12:32:07 +0100

    Update Selectionsort 1.py

--- Selectionsort 1.py
+++ Selectionsort 1.py
@@ -1,4 +1,18 @@
 #Implementiere den Selectionsort-Algorithmus und recherchiere bei Bedarf
 #eigenständig nach den benötigten Befehlen
 liste = [45,3,24,234,35,234,2412,31,42]
+lneu = []
 
+def searchLow(list, lneu):
+    helper = 
+    counter = 0
+    while counter < len(list):
+        if list[counter] < helper:
+            lneu.append(list[counter])
+            list.pop(counter)
+            break
+        if not counter == len(list) - 1:
+        	counter += 1
+        else:
+            
+	
