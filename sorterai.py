659c6eec399c0ac29835900a0aab742599a9980c
FynnHer <133028317+FynnHer@users.noreply.github.com>
2023-11-08 20:49:06 +0100

    Update sorterai.py

--- sorterai.py
+++ sorterai.py
@@ -6,14 +6,19 @@ nnfs.init()
 
 
 
-#ein Layer
+#sample Layer
 class Layer_Dense:
 
     # Layer initialisieren (beim erstellen des Layers)
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
         # weights und biases zufällige werte
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        #regularizer strenght setzen
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
 
     # normal output berechnen
     def forward(self, inputs):
@@ -24,7 +29,51 @@ class Layer_Dense:
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        #Gradients on regularization
+        #put L1 on weights
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+        #put L2 on weights
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+
+        #L1 on biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+        
+        #L2 on biases
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
+
         self.dinputs = np.dot(dvalues, self.weights.T)
+
+
+#Dropout
+
+class Layer_Dropout:
+
+    def __init__(self, rate):
+        #store and invert rate
+        self.rate = 1 - rate
+
+    def forward(self, inputs):
+
+        self.inputs = inputs
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) / self.rate
+
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
+
+
+#Activations
 
 #Sigmoid activation (0 min, 1 max)
 class Activation_Sigmoid:
@@ -35,7 +84,6 @@ class Activation_Sigmoid:
 
     def backward(self, dvalues):
         self.dinputs = dvalues * (1 - self.output) * self.output
-
 
 # ReLU activation (rectified linear unit)
 class Activation_ReLU:
@@ -53,8 +101,6 @@ class Activation_ReLU:
 
         # wenn kleiner null = 0
         self.dinputs[self.inputs <= 0] = 0
-
-
 
 # Softmax activation (e**x)
 class Activation_Softmax:
@@ -91,6 +137,7 @@ class Activation_Softmax:
             self.dinputs[index] = np.dot(jacobian_matrix,
                                          single_dvalues)
 
+#Optimizers
 
 # SGD optimizer
 class Optimizer_SGD:
@@ -148,7 +195,6 @@ class Optimizer_SGD:
     def post_update_params(self):
         self.iterations += 1
 
-
 # Adagrad optimizer
 class Optimizer_Adagrad:
 
@@ -187,7 +233,6 @@ class Optimizer_Adagrad:
 
     def post_update_params(self):
         self.iterations += 1
-
 
 # RMSprop optimizer
 class Optimizer_RMSprop:
@@ -229,8 +274,6 @@ class Optimizer_RMSprop:
 
     def post_update_params(self):
         self.iterations += 1
-
-
 
 # Adam optimizer
 class Optimizer_Adam:
@@ -309,6 +352,28 @@ class Loss:
         data_loss = np.mean(sample_losses)
 
         return data_loss
+    
+    def regularization_loss(self, layer):
+
+        regularization_loss = 0
+
+        #L1 - weights
+        #only factor > 0
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+
+        # L2 - weights
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights * layer.weights)
+
+        # L1 - bias
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases * layer.biases)
+
+        return regularization_loss
 
 
 # Cross-entropy loss funktion
@@ -352,7 +417,32 @@ class Loss_CategoricalCrossentropy(Loss):
         # gradient normalizen
         self.dinputs = self.dinputs / samples
 
+class Loss_BinaryCrossentropy(Loss):
 
+    def forward(self, y_pred, y_true):
+
+        #clip to prevent division by 0
+        #clip both sides to prevent mean
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        sample_losses = -(y_true * np.log(y_pred_clipped) + (1 - y_true) * 
+                          np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=1)
+
+        return sample_losses
+    
+    def backward(self, dvalues, y_true):
+        #number of samples
+        samples = len(dvalues)
+        #number of outputs per sample, count with first sample
+        outputs = len(dvalues[0])
+        #clip to prevent division by 0 and clip both sides to prevent mean
+        clipped_values = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        self.dinputs = -(y_true / clipped_values - (1 - y_true) /
+                         (1 - clipped_values)) / outputs
+        
+        self.dinputs = self.dinputs / samples
 
 #loss for softmax
 class Activation_Softmax_Loss_CategoricalCrossentropy():
@@ -388,36 +478,61 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
 
 
 # Dataset erstellen
+#X, y = spiral_data(samples=100, classes=2)
 X, y = spiral_data(samples=100, classes=3)
 
+# create list in list
+# from [0,0,0,0,0,...] to [[0],[0],[0],[0],...]
+#y = y.reshape(-1,1)
+
 #layer 1 mit 64 neuronen und 2 inputs
-dense1 = Layer_Dense(2, 64)
+dense1 = Layer_Dense(2, 64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
 
 # activation function ReLU
 activation1 = Activation_ReLU()
 
+dropout1 = Layer_Dropout(0.1)
 # layer 2 mit 64 neuronen (64 outputs von layer 1) und 3 outputs (3 farben)
-dense2 = Layer_Dense(64, 3)
+dense2 = Layer_Dense(64, 1)
+
+activation2 = Activation_Sigmoid()
 # 2. activation mit verbindung zu loss
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+
+#loss_function = Loss_BinaryCrossentropy()
 
 # optimizer auswählen
 #optimizer = Optimizer_SGD(decay=1e-3, momentum = 0.9)
 #optimizer = Optimizer_Adagrad(decay=1e-4)
 #optimizer = Optimizer_RMSprop(decay=1e-4)
-optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-7)
+optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-5)
+#optimizer = Optimizer_Adam(decay=5e-7)
 
 # 10001 epochen
-for epoch in range(100001):
+for epoch in range(10001):
 
     #normaler forward pass
     dense1.forward(X)
 
     activation1.forward(dense1.output)
 
-    dense2.forward(activation1.output)
+    #dropout variante
+    
+    dropout1.forward(activation1.output)
+    dense2.forward(dropout1.output)
 
-    loss = loss_activation.forward(dense2.output, y)
+
+    #binary variante
+    #dense2.forward(activation1.output)
+    #activation2.forward(dense2.output)
+
+    data_loss = loss_activation.forward(dense2.output, y)
+    #data_loss = loss_function.calculate(activation2.output, y)
+
+    regularization_loss = loss_activation.loss.regularization_loss(dense1) + loss_activation.loss.regularization_loss(dense2)
+    #regularization_loss = loss_function.regularization_loss(dense1) + loss_function.regularization_loss(dense2)
+
+    loss = data_loss + regularization_loss
 
     # Accuracy berechnen
     predictions = np.argmax(loss_activation.output, axis=1)
@@ -425,18 +540,32 @@ for epoch in range(100001):
         y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions==y)
 
+    #predictions = (activation2.output > 0.5) * 1
+    #accuracy = np.mean(predictions==y)
+
+
     #jede 100. epoche alle werte anzeigen
     if not epoch % 100:
         print(f'epoch: {epoch}, ' +
               f'acc: {accuracy:.3}, ' +
-              f'loss: {loss:.3}, ' +
+              f'loss: {loss:.3} (' +
+              f'data_loss: {data_loss:.3f}, ' +
+              f'reg_loss: {regularization_loss:.3f}), ' +
               f'lr: {optimizer.current_learning_rate}')
 
     # backward pass ausführen (partial derivatives)
     loss_activation.backward(loss_activation.output, y)
     dense2.backward(loss_activation.dinputs)
-    activation1.backward(dense2.dinputs)
+    dropout1.backward(dense2.dinputs)
+    activation1.backward(dropout1.dinputs)
     dense1.backward(activation1.dinputs)
+
+    #loss_function.backward(activation2.output, y)
+    #activation2.backward(loss_function.dinputs)
+    #dense2.backward(activation2.dinputs)
+    #activation1.backward(dense2.dinputs)
+    #dense1.backward(activation1.dinputs)
+
 
     # optimizer verändert weights und biases
     optimizer.pre_update_params()
@@ -448,10 +577,22 @@ X_test, y_test = spiral_data(samples=100, classes=3)
 dense1.forward(X_test)
 activation1.forward(dense1.output)
 dense2.forward(activation1.output)
-loss = loss_activation.forward(dense2.test, y_test)
+loss = loss_activation.forward(dense2.output, y_test)
 
-predictions ​= ​np.argmax(loss_activation.output, ​axis=​ ​1)​
-if ​len​(y_test.shape) ​== ​2​:
-	y_test ​= ​np.argmax(y_test, ​axis=​ ​1​) 
-accuracy ​= ​np.mean(predictions​==​y_test)
-print​(​f​'validation, acc: ​{accuracy​:.3f​}​, loss: ​{loss​:.3f​}​')
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y_test.shape) == 2:
+	y_test = np.argmax(y_test, axis= 1) 
+accuracy = np.mean(predictions==y_test)
+print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
+
+#X_test, y_test = spiral_data(samples=100, classes=2)
+#print(X_test, y_test)
+#y_test = y_test.reshape(-1,1)
+#dense1.forward(X_test)
+#activation1.forward(dense1.output)
+#dense2.forward(activation1.output)
+#activation2.forward(dense2.output)
+#loss = loss_function.calculate(activation2.output, y_test)
+#predictions = (activation2.output > 0.5) * 1
+#accuracy = np.mean(predictions==y_test)
+#print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
